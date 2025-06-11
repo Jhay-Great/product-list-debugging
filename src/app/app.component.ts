fbe6e9431022607eed01892652ab6f4c2d4723f2
Jummai <jummyhassan5@gmail.com>
2025-06-11 17:23:01 +0000

    create components for cart and product item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import desseretData from '../../public/data.json';
 import { Dessert } from '../models/dessert';
 import { AddToCartComponent } from './components/add-to-cart/add-to-cart.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [AddToCartComponent],
+  imports: [ProductCardComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { AddToCartComponent } from '../add-to-cart/add-to-cart.component';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [AddToCartComponent],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+})
+export class ProductCardComponent {}
